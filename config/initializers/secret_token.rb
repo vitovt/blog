3e99a537e00cfa06e2e4a48fe0a05e743aa94147
Verboloz <ruby01@GrushaLaptop.(none)>
2011-08-30 19:57:06 +0300

    Empty rails project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verbolozblog::Application.config.secret_token = '9bbd3697940b6cd4a4ae22eb35b63a460b4a2d54db59441f837f490c1626fab97df8304ec5c431eb93b5693a4db4c446974f35f0742db2c0d8e96e1dba6031e3'
